af6abcefcd331f993c1a7ec72e892d5804e7dbf0
117503445 <t117503445@gmail.com>
2025-02-06 14:27:19 +0000

    feat(ext1): 添加运行终端命令功能
    - 在 package.json 中添加了新的命令 ext1.runInTerminal
    - 在 extension.ts 中实现了 ext1.runInTerminal 命令的功能
    - 在资源管理器上下文菜单中添加了运行终端命令的选项
    
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- ext1/src/extension.ts
+++ ext1/src/extension.ts
@@ -30,14 +30,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ext1.helloWorld', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('ext1.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from ext1!!! 211426');
 		printChannelOutput('Hello World from ext1, 220927');
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	context.subscriptions.push(vscode.commands.registerCommand('ext1.runInTerminal', (resourceUri) => {
+		printChannelOutput('runInTerminal');
+	}));
 }
 
 // This method is called when your extension is deactivated
