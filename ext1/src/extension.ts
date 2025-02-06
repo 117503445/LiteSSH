d3d43d1980c38118d54c6a729b58e52d050cc3e3
117503445 <t117503445@gmail.com>
2025-02-06 14:12:45 +0000

    feat(ext1): 添加输出通道打印功能并优化代码
    - 在 extension.ts 中添加了输出通道打印功能
    - 更新 package.json，在 lint 命令中增加 --fix 参数
    
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- ext1/src/extension.ts
+++ ext1/src/extension.ts
@@ -2,9 +2,26 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let outputChannel: vscode.OutputChannel;
+
+/**
+ * Prints the given content on the output channel.
+ *
+ * @param content The content to be printed.
+ * @param reveal Whether the output channel should be revealed.
+ */
+export const printChannelOutput = (content: string, reveal = false): void => {
+	outputChannel.appendLine(content);
+	if (reveal) {
+		outputChannel.show(true);
+	}
+};
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	outputChannel = vscode.window.createOutputChannel("My Extension");
+
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,10 +34,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from ext1!!! 211426');
+		printChannelOutput('Hello World from ext1, 220927');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
