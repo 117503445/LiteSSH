94876649260ca349f096f2a1d022b52047f368fb
117503445 <t117503445@gmail.com>
2025-02-06 14:03:11 +0000

    refactor(ext1): 更新扩展信息和功能
    - 在 package.json 中添加 publisher 和 repository 信息
    - 修改 extension.ts 中的 Hello World 消息
    
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- ext1/src/extension.ts
+++ ext1/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('ext1.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ext1!');
+		vscode.window.showInformationMessage('Hello World from ext1!!! 211426');
 	});
 
 	context.subscriptions.push(disposable);
